7054bca6adead550beb57c77732595cc522d97ca
vishakha <vishakhaj.18@gmail.com>
2017-05-14 17:34:22 +0200

    Initialized spring boot application

---
+++ wishlistservice/src/main/java/com/wishlistservice/WishlistserviceApplication.java
@@ -0,0 +1,12 @@
+package com.wishlistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlistserviceApplication.class, args);
+	}
+}
---
+++ wishlistservice/src/test/java/com/wishlistservice/wishlistservice/WishlistserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wishlistservice.wishlistservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WishlistserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
